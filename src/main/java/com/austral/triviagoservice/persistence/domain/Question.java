b802177bd2b4d626b0016d0a4a588360884efb6a
Pedro <petocasti@gmail.com>
2023-08-17 19:35:36 -0300

    Question Entity created

---
+++ src/main/java/com/austral/triviagoservice/persistence/domain/Question.java
@@ -0,0 +1,41 @@
+package com.austral.triviagoservice.persistence.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Question {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Long quizId;
+
+    private String content;
+
+    public Question(Long quizId, String content) {
+        this.quizId = quizId;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getQuizId() {
+        return quizId;
+    }
+
+    public void setQuizId(Long quizId) {
+        this.quizId = quizId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
