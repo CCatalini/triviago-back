5f804f7ad22ce120f99b6dc115cb199f94971578
Dimi <stefangeorges4799@gmail.com>
2023-08-17 16:48:35 -0300

    Initial commit

---
+++ src/main/java/com/austral/triviagoservice/TriviagoserviceApplication.java
@@ -0,0 +1,13 @@
+package com.austral.triviagoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriviagoserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriviagoserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/austral/triviagoservice/TriviagoserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.austral.triviagoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriviagoserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
