a10ff9e7a017e985c0bae625faf19b7687d47fb7
Pedro <petocasti@gmail.com>
2023-08-17 19:33:43 -0300

    Comment Entity created

---
+++ src/main/java/com/austral/triviagoservice/persistence/domain/Comment.java
@@ -0,0 +1,66 @@
+package com.austral.triviagoservice.persistence.domain;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+
+public class Comment {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Long userId;
+
+    private Long quizId;
+
+    private String content;
+
+    private LocalDateTime creationDateTime;
+
+    public Comment(Long userId, Long quizId, String content) {
+        this.userId = userId;
+        this.quizId = quizId;
+        this.content = content;
+        this.creationDateTime = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getQuizId() {
+        return quizId;
+    }
+
+    public void setQuizId(Long quizId) {
+        this.quizId = quizId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getCreationDateTime() {
+        return creationDateTime;
+    }
+
+    public void setCreationDateTime(LocalDateTime creationDateTime) {
+        this.creationDateTime = creationDateTime;
+    }
+}
