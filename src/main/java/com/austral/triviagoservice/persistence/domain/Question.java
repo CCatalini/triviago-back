4caf77f3cf10d3518b3448d5856d1ac6ca3710ec
Pedro <petocasti@gmail.com>
2023-08-22 14:14:26 -0300

    Fixed mistake on generated value

--- src/main/java/com/austral/triviagoservice/persistence/domain/Question.java
+++ src/main/java/com/austral/triviagoservice/persistence/domain/Question.java
@@ -2,12 +2,13 @@ package com.austral.triviagoservice.persistence.domain;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Question {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private Long quizId;
