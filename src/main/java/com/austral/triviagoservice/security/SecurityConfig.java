557e6ab4c2a63953894668dd8551288a0200de72
SantiGrimoldi <Santiago.grimoldi@ing.austral.edu.ar>
2023-08-30 12:39:03 -0300

    cambios intermedios

--- src/main/java/com/austral/triviagoservice/security/SecurityConfig.java
+++
@@ -1,9 +0,0 @@
-//package com.austral.triviagoservice.security;
-//
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-//@EnableWebSecurity(debug = true)
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//}
