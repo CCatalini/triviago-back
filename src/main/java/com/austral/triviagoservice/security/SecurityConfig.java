686c5336b1170bdbb481c320a0b3d03072e7f1fd
SantiGrimoldi <Santiago.grimoldi@ing.austral.edu.ar>
2023-08-30 14:19:08 -0300

    Created Dto corrected some other things

--- src/main/java/com/austral/triviagoservice/security/SecurityConfig.java
+++ src/main/java/com/austral/triviagoservice/security/SecurityConfig.java
@@ -1,11 +1,8 @@
 package com.austral.triviagoservice.security;
 
-import com.austral.triviagoservice.business.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,38 +10,21 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import javax.servlet.Filter;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration {
-
-    private final JWTAuthenticationFilter JWTAuthenticationFilter;
-    private final UserService userService;
-
-    public SecurityConfiguration
-            (JWTAuthenticationFilter jwtAuthenticationFilter,
-             UserService userService) {
-        this.JWTAuthenticationFilter = jwtAuthenticationFilter;
-        this.userService = userService;
-
-    }
+public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .authorizeHttpRequests()
-                .antMatchers("api/auth/**").permitAll()
+                .antMatchers("/auth/**").permitAll()
                     .anyRequest().authenticated()
                 .and()
                 .sessionManagement()
-                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .addFilterBefore(JWTAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         return http.build();
     }
