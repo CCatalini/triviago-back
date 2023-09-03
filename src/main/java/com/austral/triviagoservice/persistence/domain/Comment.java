df980a1b51e93843ca9c46c80c6e7574e917d01c
Pedro <petocasti@gmail.com>
2023-09-03 18:40:15 -0300

    Corregida una exepcion que permitia null en la db

--- src/main/java/com/austral/triviagoservice/persistence/domain/Comment.java
+++ src/main/java/com/austral/triviagoservice/persistence/domain/Comment.java
@@ -1,10 +1,7 @@
 package com.austral.triviagoservice.persistence.domain;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
@@ -12,9 +9,9 @@ public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Column(nullable = false)
     private Long userId;
-
+    @Column(nullable = false)
     private Long quizId;
 
     private String content;
