4753578acefba20f836ead4a6561748533fb2738
marcos <marcos.lambertini@ing.austral.edu.ar>
2023-09-18 19:25:25 -0300

    refactor service interface

--- src/main/java/com/austral/triviagoservice/business/CommentLikeService.java
+++ src/main/java/com/austral/triviagoservice/business/CommentLikeService.java
@@ -1,11 +1,11 @@
 package com.austral.triviagoservice.business;
 
 import com.austral.triviagoservice.business.exception.InvalidContentException;
+import com.austral.triviagoservice.business.exception.NotFoundException;
 import com.austral.triviagoservice.persistence.domain.CommentLike;
 
 public interface CommentLikeService {
 
-    CommentLike creat(CommentLike commentLike);
-    CommentLike getById(Long id) throws InvalidContentException;
-    void save(CommentLike commentLike);
+    CommentLike create(CommentLike commentLike);
+    CommentLike getById(Long id) throws InvalidContentException, NotFoundException;
 }
