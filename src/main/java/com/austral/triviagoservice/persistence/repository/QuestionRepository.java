a088e1c207f3eaa34bd9aa7d1633919f4244556a
Pedro <petocasti@gmail.com>
2023-09-21 20:41:31 -0300

    Moved questionRepository to the correct package

--- src/main/java/com/austral/triviagoservice/persistence/repository/QuestionRepository.java
+++ src/main/java/com/austral/triviagoservice/persistence/repository/QuestionRepository.java
@@ -1,5 +1,6 @@
-package com.austral.triviagoservice.persistence.domain;
+package com.austral.triviagoservice.persistence.repository;
 
+import com.austral.triviagoservice.persistence.domain.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
