d2e8b65fe83603a2159ef7257b69da877196b6cd
marcos <marcos.lambertini@ing.austral.edu.ar>
2023-09-29 16:41:30 -0300

    QuizRating repository

---
+++ src/main/java/com/austral/triviagoservice/persistence/repository/QuizRatingRepository.java
@@ -0,0 +1,9 @@
+package com.austral.triviagoservice.persistence.repository;
+
+import com.austral.triviagoservice.persistence.domain.QuizRating;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuizRatingRepository extends JpaRepository<QuizRating, Long> {
+}
