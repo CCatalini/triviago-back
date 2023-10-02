f8797d35dc6f25fe6d130b800591197b965e5958
SantiGrimoldi <Santiago.grimoldi@ing.austral.edu.ar>
2023-10-02 15:50:02 -0300

    added getters and setters to UserDto

--- src/main/java/com/austral/triviagoservice/presentation/dto/UserDto.java
+++ src/main/java/com/austral/triviagoservice/presentation/dto/UserDto.java
@@ -2,11 +2,15 @@ package com.austral.triviagoservice.presentation.dto;
 
 import com.austral.triviagoservice.persistence.domain.CommentLike;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Builder
+@Setter
+@Getter
 public class UserDto {
     private Long id;
     private String email;
