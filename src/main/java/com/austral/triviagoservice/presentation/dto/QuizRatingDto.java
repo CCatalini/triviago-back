786f5a0d455c4432fe228aab67b9fdcefa42fbd5
marcos <marcos.lambertini@ing.austral.edu.ar>
2023-10-05 21:32:40 -0300

    fix

--- src/main/java/com/austral/triviagoservice/presentation/dto/QuizRatingDto.java
+++ src/main/java/com/austral/triviagoservice/presentation/dto/QuizRatingDto.java
@@ -2,8 +2,10 @@ package com.austral.triviagoservice.presentation.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class QuizRatingDto {
 
     private Integer rating;
