548c5c83d080e79188b6cd756505d1256ae2f95f
SantiGrimoldi <Santiago.grimoldi@ing.austral.edu.ar>
2023-10-13 14:06:32 -0300

    created the QuizResolution entity

---
+++ src/main/java/com/austral/triviagoservice/persistence/domain/QuizResolution.java
@@ -0,0 +1,32 @@
+package com.austral.triviagoservice.persistence.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Getter
+@Setter
+@Builder
+public class QuizResolution {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long userId;
+    private Long quizId;
+    private int correctAnswers;
+    private LocalDateTime resolutionDateTime;
+
+    public QuizResolution(){}
+
+
+
+}
