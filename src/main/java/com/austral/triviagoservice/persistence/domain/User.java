101e88653dc96eeff4a0525d7b12115972528c18
Pedro <petocasti@gmail.com>
2023-11-02 20:13:29 -0300

    Añadido otro cascade para la eliminacion correcta

--- src/main/java/com/austral/triviagoservice/persistence/domain/User.java
+++ src/main/java/com/austral/triviagoservice/persistence/domain/User.java
@@ -38,7 +38,7 @@ public class User{
     @OneToMany(mappedBy="user", fetch = FetchType.EAGER)
     private List<CommentLike> likes;
 
-    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY,cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonIgnore
     List<QuizRating> ratings;
 
